7062b3a5131013f18d8741d9674556bb8714bae8
Niccolo Primo <nprimo@c2r8s2.42lisboa.com>
2022-08-31 20:44:41 +0100

    add update_ray to test collision with hor lines

--- headers/ft_util.h
+++ headers/ft_util.h
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 20:38:14 by edos-san          #+#    #+#             */
-/*   Updated: 2022/08/28 20:09:19 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/08/31 20:09:30 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,8 @@
 # define KEY_LEFT 65361
 # define KEY_RIGHT 65363
 # define MOVE_LEN 10
-# define W_WIDTH 1200
-# define W_HEIGHT 1000
+# define W_WIDTH 1080
+# define W_HEIGHT 860
 # define VIEW_ANGLE 60
 # define N_RAYS	100
 # define COLOR_TRANSPARENT 0xd411aa
--- src/components/raycast/ft_raycast.c
+++ src/components/raycast/ft_raycast.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <nprimo@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 21:57:49 by ezequeil          #+#    #+#             */
-/*   Updated: 2022/08/31 19:51:05 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/08/31 20:43:08 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,6 @@ void	print_column(double distance_hor, int rel_angle)
 
 void	update_print_ray(t_vector p, double rel_angle, int max)
 {
-	double		val;
 	t_vector	delta;
 	double		i;
 
@@ -64,6 +63,52 @@ void	update_print_ray(t_vector p, double rel_angle, int max)
 	}
 }
 
+void update_ray(t_vector p, double rel_anlge, int max_loop)
+{
+	t_vector	cross;
+	t_vector	offset;
+	t_vector	check_pos;
+	double		angle;
+	int			loop;
+	
+	cross = vector_zero();
+	offset = vector_zero();
+	loop = -1;
+	angle = p.angle + rel_anlge;
+	printf("Angle: %f\n", angle);
+	cross.x = -(p.y - cross.y) / ft_tan(angle) + p.x;
+	if (angle > 180) // is looking down
+	{
+		cross.y = ((int) p.x / GRID_SIZE) * GRID_SIZE - 0.0001;
+		offset.y = +GRID_SIZE;
+	}
+	if (angle < 180) // is looking up
+	{
+		cross.y = ((int) p.x / GRID_SIZE) * GRID_SIZE + GRID_SIZE;
+		offset.y = -GRID_SIZE;
+	}
+	offset.x = offset.y / ft_tan(angle);
+	if (angle == 180 || angle == 0 || angle == 360)
+		loop = max_loop;
+	printf("Player pos: %d %d\n", (int) p.x / GRID_SIZE, (int) p.y / GRID_SIZE);
+	while(++loop < max_loop)
+	{
+		check_pos.x = (int) cross.x / GRID_SIZE;
+		check_pos.y = (int) cross.y / GRID_SIZE;
+		if (check_pos.x > 0 && check_pos.y > 0
+			&& map()->maps[(int) check_pos.x, (int) check_pos.y]
+			&& map()->maps[(int) check_pos.x, (int) check_pos.y] == '1')
+			loop = max_loop;
+		else
+		{
+			cross.x += offset.x;
+			cross.y += offset.y;
+		}
+	}
+	printf("loop: %d\n", loop);
+	printf("Check pos: %d %d\n", (int) check_pos.x , (int) check_pos.y);
+}
+
 void	*print_raycast(t_player *p)
 {
 	double		rel_angle;
@@ -75,6 +120,8 @@ void	*print_raycast(t_player *p)
 	{
 		update_print_ray(p->vector, rel_angle, 500);
 		rel_angle--;
+		if (rel_angle == 0)
+			update_ray(p->vector, rel_angle, 10);
 	}
 	return (ray_return);
 }
