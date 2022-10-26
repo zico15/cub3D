23fc0ea055f8586995deef93bc3cb83d0be62da5
Ezequiel carlos Dos santos <edos-san@c2r10s3.42lisboa.com>
2022-10-26 15:31:17 +0100

    chech_map

---
+++ src/objects/map/checks/ft_check_case.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_case.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edos-san <edos-san@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/26 15:14:54 by edos-san          #+#    #+#             */
+/*   Updated: 2022/10/26 15:27:21 by edos-san         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ft_check.h>
+
+int	check_borde(t_map *m, int x, int y)
+{
+	char	c;
+	int		size_x;
+	int		size_y;
+
+	size_x = string().size(m->check_maps[y]) - 1;
+	size_y = m->size_height - 1;
+	c = m->check_maps[y][x];
+	m->count_player += (string().contains("NSWE", _str(c)) > 0);
+	if ((x > 0 && x < size_x) && (y > 0 && y < size_y))
+		return (1);
+	return (c == '1');
+}
+
+char	check_case_ini(t_map *map, int x, int y)
+{
+	char	c;
+	int		size;
+
+	size = string().size(map->check_maps[y]);
+	if (y < 0 || y >= map->size_height)
+		return (0);
+	if (x < 0 || x >= size)
+		return (0);
+	c = map->check_maps[y][x];
+	if (!check_borde(map, x, y))
+	{
+		map->is_map_ok = 0;
+		return (0);
+	}
+	if (c != '0' && !engine()->new_obs[(int) c])
+	{
+		map->is_map_ok = 0;
+		return (0);
+	}
+	return (c);
+}
+
+int	check_pos_init(t_map *m, int x, int y)
+{
+	int	c;
+
+	if (!m->is_map_ok)
+		return (0);
+	c = (int) check_case_ini(m, x + 1, y);
+	if (!c || (c != '0' && !engine()->new_obs[c]))
+		return (0);
+	c = (int) check_case_ini(m, x - 1, y);
+	if (!c || (c != '0' && !engine()->new_obs[c]))
+		return (0);
+	c = (int) check_case_ini(m, x, y + 1);
+	if (!c || (c != '0' && !engine()->new_obs[c]))
+		return (0);
+	c = (int) check_case_ini(m, x, y - 1);
+	if (!c || (c != '0' && !engine()->new_obs[c]))
+		return (0);
+	return (1);
+}
